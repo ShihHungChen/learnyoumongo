5e6fd11c2a603c7036b715a850149e99f7428946
Shi Hung <c.shihhung@gmail.com>
2017-05-28 16:04:22 +0000

    Completed lesson five

--- solution.js
+++ solution.js
@@ -1,23 +1,16 @@
-//console.log(process.argv);
 var mongo = require('mongodb').MongoClient;
 mongo.connect('mongodb://localhost:27017/learnyoumongo', function(err, db){
     if(err){
-        db.close();
-        return console.error('There was an error : ', err);
+        throw err;
     }
     else{
-        var collection = db.collection('parrots');
-        collection.find({
-            age : {$gt : parseInt(process.argv[2])}
-        },{
-            name : 1,
-            age : 1,
-            _id : 0
-        }).toArray(function(err, documents){
+        var collection = db.collection('docs');
+        var insert_data = {firstName: process.argv[2], lastName:process.argv[3]};
+        collection.insert(insert_data,function(err, data){
             if(err){
-                return console.error('There was an error : ', err);
+                throw err;
             }
-            console.log(documents);
+            console.log(JSON.stringify(insert_data));
         })
     }
     db.close();
