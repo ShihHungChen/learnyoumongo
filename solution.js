9f3735ac0a6be89b103bfcc48c4899a3c3703750
Shi Hung <c.shihhung@gmail.com>
2017-05-29 10:17:37 +0000

    Completed lesson seven

--- solution.js
+++ solution.js
@@ -2,14 +2,8 @@ var mongo = require('mongodb').MongoClient;
 mongo.connect('mongodb://localhost:27017/' + process.argv[2], function(err, db){
     if(err) throw err;
     
-    var collection = db.collection('users');
-    collection.update({
-        username:'tinatime'
-    },{
-        $set: {
-            age:40
-        }
-    }, function(err, data){
+    var collection = db.collection(process.argv[3]);
+    collection.remove({ _id : process.argv[4]}, function(err ,data){
         if(err) throw err;
         db.close();
     })
