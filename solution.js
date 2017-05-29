e55321e96a42d0fe259b369684740a1a489e2f4e
Shi Hung <c.shihhung@gmail.com>
2017-05-29 10:13:34 +0000

    Completed lesson six

--- solution.js
+++ solution.js
@@ -1,17 +1,16 @@
 var mongo = require('mongodb').MongoClient;
-mongo.connect('mongodb://localhost:27017/learnyoumongo', function(err, db){
-    if(err){
-        throw err;
-    }
-    else{
-        var collection = db.collection('docs');
-        var insert_data = {firstName: process.argv[2], lastName:process.argv[3]};
-        collection.insert(insert_data,function(err, data){
-            if(err){
-                throw err;
-            }
-            console.log(JSON.stringify(insert_data));
-        })
-    }
-    db.close();
+mongo.connect('mongodb://localhost:27017/' + process.argv[2], function(err, db){
+    if(err) throw err;
+    
+    var collection = db.collection('users');
+    collection.update({
+        username:'tinatime'
+    },{
+        $set: {
+            age:40
+        }
+    }, function(err, data){
+        if(err) throw err;
+        db.close();
+    })
 })
