9813b092d7d61ca5c249f50206aa3d21ced18d50
Shi Hung <c.shihhung@gmail.com>
2017-05-29 12:57:24 +0000

    Completed all lessons

--- solution.js
+++ solution.js
@@ -3,12 +3,13 @@ mongo.connect('mongodb://localhost:27017/learnyoumongo', function(err, db){
     if(err){ 
         throw err;
     }
-    var collection = db.collection('parrots');
-    collection.count({ age : {$gt : Number(process.argv[2])}}, function(err ,count){
-        if(err){
-            throw err;
-        }
-        console.log(count);
-        db.close();
-    })
+    var collection = db.collection('prices');
+    collection.aggregate([
+        {$match:{size:process.argv[2]}},
+        {$group :{ _id:'average',average:{$avg : '$price'}}} //use '$' to specified property, if not use it will create new property
+        ]).toArray(function(err, results){
+            if(err) throw err;
+            console.log(Number(results[0].average).toFixed(2));
+        })
+    db.close();
 })
